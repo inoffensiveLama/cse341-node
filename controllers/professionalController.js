2249d2acae78f6f46e6acf1083ecca1bd1576629
janZ <janzander96@gmail.com>
2024-09-20 07:17:59 +0800

    removed console log

--- controllers/professionalController.js
+++ controllers/professionalController.js
@@ -5,7 +5,6 @@ const mongodb = require("../db/connect");
 professionalController.getData = async (req, res, next) => {
     const result = await mongodb.getDb().db().collection("user").find();
     result.toArray().then((lists) => {
-        console.log(JSON.stringify(lists[0]));
         res.setHeader("Content-Type", "application/json");
         res.status(200).json(lists[0]);
     });
